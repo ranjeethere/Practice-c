9dc27eb2628a5b2bfaca832d1d0ffae65f18618b
Ranjeet Yadav <147843523+ranjeethere@users.noreply.github.com>
2023-12-04 20:39:31 +0530

    Add files via upload

---
+++ fun_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+float PrintSquare(float n)
+{
+    return n*n;
+}
+int main()
+{
+    //int r = 20;
+    // int ans = PrintSquare(20);
+    printf("The square of 20 is : %f",PrintSquare(20));
+
+
+}
