8480ab21261b1cd86dce04687e3fafe1a779cc95
Ranjeet Yadav <147843523+ranjeethere@users.noreply.github.com>
2024-01-02 23:39:20 +0530

    Add files via upload

---
+++ 21.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[20] = "ranjeet";
+    int len = 0;
+    //for(int i = 0;str[i]!='\0';++i)
+    while(str[len]!='\0')
+    {
+        len ++;
+       // printf("\n length of the string are %d",len);
+    }
+  
+    printf("\n length of the string are %d",len);
+        //return 0;
+  
+   
+return 0;
+
+}
