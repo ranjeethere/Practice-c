53ea7bd12235ce0d2b8e8a77f9184acdb51d2d73
Ranjeet Yadav <147843523+ranjeethere@users.noreply.github.com>
2024-01-07 13:56:09 +0530

    Add files via upload

---
+++ 24.c
@@ -0,0 +1,18 @@
+//C Program to Check if two strings are same or not: 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[10] = "ranjeet";
+    char str2[10] = "yadav";
+    if(str1 == str2)
+    {
+        printf("both string are same : ");
+
+    }
+    else {
+        printf("Both string are not same :");
+    }
+    return 0;
+
+}
